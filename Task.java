9814fe20b2a37f25b0692cf1591eadbc0a300575
*vadim12121212* <imailcom8@gmail.com>
2023-07-02 10:41:18 +0300

    Задача

---
+++ Task.java
@@ -0,0 +1,20 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Task {
+    public static void main(String[] args) {
+        // заполняем массив
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Введите количество элементов массива: ");
+        int n = sc.nextInt();
+        int[] newArr = new int[n];
+        System.out.println("Введите элементы массива: ");
+        for (int i = 0; i < n; i++) {
+            newArr[i] = sc.nextInt();
+        }
+        System.out.println("Исходный массив: " + Arrays.toString(newArr));
+        // сортируем массив
+        int[] filterArr = Arrays.stream(newArr).filter(i -> i % 2 != 0).toArray();
+        System.out.println("Полученный массив: " + Arrays.toString(filterArr));
+    }
+}
